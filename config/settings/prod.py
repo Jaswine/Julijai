a64aa099469f2cee45c4d06da9c9af5043de7ba0
Vaseles <vaselesstolarov813@gmail.com>
2022-08-02 12:19:29 +0600

    Add heroku settings to production

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -10,6 +10,37 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = [
-  'zackp-basic-blog.herokuapp.com',
-  'zackp.blog',
+  'julijai-it-hub.herokuapp.com',
+  'julijai.blog',
 ]
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
+LOGGING = {
+  'version':1,
+  'disable_existing_loggers': False,
+  'formatters': {
+    'verbose': {
+      'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+      'datefmt': "%d/%b/%Y %H:%M:%S"
+    },
+    'simple': {
+      'format': '%(levelname)s %(message)s'
+    },
+  },
+  'handlers': {
+    'console': {
+      'level': 'DEBUG',
+      'class': 'loging.StreamHandler',
+    },
+  },
+  'loggers': {
+    'MYAPP': {
+      'handlers': ['console'],
+      'level': 'DEBUG',
+    },
+  }
+}
+
+django_on_heroku.settings(locals(), staticfiles=False)
+del DATABASES['default']['OPTIONS']['sslmode']
